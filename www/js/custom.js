b4c577008aa4ab79ad03ab0b6ed3cba6d658450b
Thomas Tursics <thomas@tursics.de>
2014-06-16 09:59:07 +0200

    added custom.js

---
+++ www/js/custom.js
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------------
+/* custom.js */
+// -----------------------------------------------------------------------------
+
+$( document).on( "pagecreate", "#pageHome", function()
+{
+});
+
+// -----------------------------------------------------------------------------
+
+$( document).on( "pageshow", "#pageHome", function()
+{
+		var txt = '';
+		txt += '<div>Wähle:</div>';
+
+		txt += '<ul data-role="listview" data-theme="a" data-inset="true">';
+//		txt += '<li data-role="list-divider">Bestimmungsschlüssel</li>';
+		txt += '<li data-icon="false"><a href="#pageCopyright">Häufige Vögel in Gärten und Siedlungen</a></li>';
+		txt += '<li data-icon="false"><a href="#pageCopyright">Amphibien und Reptilien</a></li>';
+		txt += '<li data-icon="false"><a href="#pageCopyright">Holzige Pflanzen in Deutschland</a></li>';
+		txt += '</ul>';
+
+		$( '#pageHomeContent').html( txt);
+		$( '#pageHomeContent').trigger( 'create');
+		$( '#pageHomeContent').trigger( 'updatelayout');
+});
+
+// -----------------------------------------------------------------------------
